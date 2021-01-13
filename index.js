5cfcb30fb03182865e7f78d3791debca25101be2
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-12 17:13:24 -0700

    main: Call google

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // Dependencies
 const express = require('express');
-// const axios = require('axios');
+const axios = require('axios');
 require('dotenv').config();
 
 // Initialize our express server
 const app = express();
 
-
 // Write out a home route (stub)
-
 app.get('/', (req, res) => {
-  res.send('Hello');
+  axios.get('http://www.google.com')
+    .then(response => {
+      console.log(`response: `, response);
+    });
 });
 
 // call app
