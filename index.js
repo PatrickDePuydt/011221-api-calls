e323bbbd7aa15f89c00a147c6115211752cfec01
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-12 17:02:16 -0700

    main: stub out routes

---
+++ index.js
@@ -0,0 +1,18 @@
+// Dependencies
+const express = require('express');
+// const axios = require('axios');
+require('dotenv').config();
+
+// Initialize our express server
+const app = express();
+
+// Write out a home route (stub)
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+// call app
+app.listen(3000, () => {
+  console.log(`🛺 Listening`);
+})
